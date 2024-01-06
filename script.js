f1480be69e5bbf30e6805e3dde841b3e271832d4
JohnnyWLan <130187009+JohnnyWLan@users.noreply.github.com>
2024-01-05 21:30:10 -0600

    ready to go

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+var _= require('lodash');
+ let array = [1,2,3,4];
+ console.log('answer:',_.without(array, 3));   
 let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
+let button = document.querySelector("button");''
 
-console.log(css);
-console.log(color1);
-console.log(color2);
+function setGradient() {
+     body.style.background =
+     "linear-gradient(to right, "
+    +color1.value
+    + ", " 
+    + color2.value
+     + ")";
+     css.textContent = body.style.background + ";";
+}
+function setRandomGradient() {
+  body.style.background =
+    "linear-gradient(to right, " +
+    generateRandomColors() +
+    ", " +
+    generateRandomColors();
 
-color1.addEventListener("input", function(){
-    console.log(color1.value);
-}) 
+  css.textContent = body.style.background + ";";
+}
 
-color2.addEventListener("input", function(){
-    console.log(color2.value);
-})
+function generateRandomColors() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
 
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+button.addEventListener("click", setRandomGradient);
